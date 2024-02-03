5ddbb195db3f36405ccdbc25836f11c3d305b32e
Wahaj Sajid <mwahajsajidali@gmail.com>
2024-02-03 11:25:47 +0500

    Designing Report Fragment

--- app/src/main/java/com/example/about/GPA.kt
+++ app/src/main/java/com/example/about/GPA.kt
@@ -1,19 +1,5 @@
 package com.example.about
 
 data class GPA( var finalGPA: String = "")
-data class Marks(var marks: Array<String> = arrayOf("","","","","","")) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Marks
-
-        return marks.contentEquals(other.marks)
-    }
-
-    override fun hashCode(): Int {
-        return marks.contentHashCode()
-    }
-}
 
 
