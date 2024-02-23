bf8da94f06432ce9da412482bca0e9d3ea8123f1
Wahaj Sajid <mwahajsajidali@gmail.com>
2024-02-23 22:18:52 +0500

    Removed Warnings in layout files

--- app/src/main/java/com/example/about/SplashScreen.kt
+++ app/src/main/java/com/example/about/SplashScreen.kt
@@ -4,11 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.appbar.AppBarLayout
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        AppBarLayout.GONE
         Handler(mainLooper).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             finish()
